0dcbcb8b7f91ae9d0ed029b09a41eae90d5b48e2
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-15 17:13:18 -0700

    fix

--- src/ts-utils.ts
+++ src/ts-utils.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License in the project root for license information.
 
-export dddconst asNonUndefined = <T>(v: T|undefined) => v as T
+export const asNonUndefined = <T>(v: T|undefined) => v as T
