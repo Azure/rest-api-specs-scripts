53fed22d4ed38d4061adc6543aa2dc09a08268c7
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-17 11:46:37 -0700

    0.2.1

--- src/momentOfTruthUtils.ts
+++ src/momentOfTruthUtils.ts
@@ -3,8 +3,36 @@
 
 import * as stringMap from '@ts-common/string-map'
 
+export type Issue = {
+  readonly type?: string
+  readonly code: unknown
+  readonly message: unknown
+  readonly id: string
+  readonly validationCategory: string
+  readonly providerNamespace: unknown
+  readonly resourceType: unknown
+  readonly sources: readonly unknown[]
+  readonly jsonref: string
+  readonly filePath: string
+  readonly lineNumber: number
+}
+
+export type File = {
+  readonly before: readonly Issue[]
+  readonly after: readonly Issue[]
+}
+
 export type FinalResult = {
   readonly pullRequest: unknown,
   readonly repositoryUrl: unknown,
   readonly files: stringMap.MutableStringMap<stringMap.MutableStringMap<unknown>>
 }
+
+/*
+type JsonData = {
+    readonly files: stringMap.StringMap<{
+        readonly before: readonly Issue[]
+        readonly after: readonly Issue[]
+    }>
+}
+*/
