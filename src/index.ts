6e28b58f1913be0b7c74a96a1950161f69d09154
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-17 15:24:56 -0700

    revert breaking changes.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License in the project root for license information.
 
-import * as breakingChange from './breaking-change'
-import * as momentOfTruth from './momentOfTruth'
-import * as utils from './utils'
+export { runScript as breakingChange } from './breaking-change'
+export { runScript as momentOfTruth } from './momentOfTruth'
 
-export { breakingChange, momentOfTruth, utils }
+import * as utils from './utils'
+export { utils }
