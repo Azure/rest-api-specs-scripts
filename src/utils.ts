dd1aad6859b31cebb8b93d267b2370e7e796c76d
Nelson Daniel Troncoso <nd.ta@hotmail.com>
2019-07-02 15:45:49 -0700

    change from jsonStructuralDiff to structuralDiff

--- src/utils.ts
+++ src/utils.ts
@@ -273,10 +273,7 @@ export const getFilesChangedInPR = async (pr: devOps.PullRequestProperties | und
   let result = getSwaggers();
   if (pr !== undefined) {
     try {
-      let jsonFilesChanged = new Set<string>(await pr.jsonStructuralDiff().toArray());
-      let filesChanged = (await pr.diff())
-        .map(file => file.path)
-        .filter(path =>  !path.endsWith('.json') || (path.endsWith('.json') && jsonFilesChanged.has(path)))      
+      let filesChanged = await pr.structuralDiff().toArray()
 
       console.log('>>>>> Files changed in this PR are as follows:')
       console.log(filesChanged);
