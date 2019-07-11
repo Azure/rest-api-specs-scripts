3da86fe62e9b705285ec89bf60e31693de1de146
ruowan <52271048+ruowan@users.noreply.github.com>
2019-07-11 08:11:23 +0800

    Replace diff function in `getFilesChangedInPR` and small fix (#11)
    * `structuralDiff` in `getFilesChangedInPR` instead of `diff`
    
    * fix '@types/jsonpath' and result for exec function
    
    * bump version small fix

--- src/modelValidation.ts
+++ src/modelValidation.ts
@@ -20,14 +20,14 @@ const exec = (cmd: string, options?: cp.SpawnSyncOptions) => {
 export async function main() {
   const pr = await devOps.createPullRequestProperties(cli.defaultConfig())
   const swaggersToProcess = await utils.getFilesChangedInPR(pr);
-  let result = 0
+  let result: number = 0
   for (const swagger of swaggersToProcess) {
     try {
       // await oav.validateExamples(swagger, null, {consoleLogLevel: 'error', pretty: true});
       // run OAV as a separate process to avoid memory issues.
       const r = exec(`node node_modules/oav/dist/cli.js validate-example ${swagger} --pretty`)
       if (result === 0) {
-        result = r
+        result = r === null ? 1 : r
       }
     } catch (e) {
       console.error("error: ")
