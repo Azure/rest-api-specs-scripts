a7856ae9b54c916e2b015045e1fbb7b77b00952b
Jianye Xi <jianye.xi@microsoft.com>
2020-03-04 10:47:02 +0800

    fix octokit.issues undefined issue

--- src/postToGitHub.ts
+++ src/postToGitHub.ts
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-const { octokit } = require('@octokit/rest');
+const { Octokit } = require('@octokit/rest');
+const octokit = new Octokit();
+ 
 let token = process.env.GITHUB_TOKEN;
 
 if(token != undefined) {
