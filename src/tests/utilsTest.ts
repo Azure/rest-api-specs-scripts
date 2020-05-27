832f8cccd224d72079d726755225bb05ddd56265
Ruoxuan Wang <52271048+ruowan@users.noreply.github.com>
2020-05-27 13:00:58 +0800

    unified pipeline output (#49)
    bump beta version
    
    add trimSwaggerPath
    
    bump version
    
    append to log file
    
    one json log one line.
    
    fix env
    
    github style path
    
    upgrade oad to 0.7.1
    
    collect error log
    
    update package, small update about format
    
    bump version and update changelog
    
    rename function name.Breaking change remove resolve swagger since oad
    already do it.
    i# Please enter the commit message for your changes. Lines starting
    
    bump version

--- src/tests/utilsTest.ts
+++ src/tests/utilsTest.ts
@@ -1,52 +1,66 @@
-import { cleanUp } from './helper';
+import { cleanUp } from "./helper";
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License in the project root for license information.
 
 import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
-import { devOps } from '@azure/avocado';
+import { devOps } from "@azure/avocado";
 import * as assert from "assert";
-import {utils as utils} from "../index"
-import {createDevOpsEnv} from "./helper"
-import * as fs from 'fs-extra'
-import {getTagsFromChangedFile,isTagExisting,getChangeFilesReadmeMap} from "../utils"
-
+import { utils } from "../index";
+import { createDevOpsEnv } from "./helper";
+import * as fs from "fs-extra";
+import {
+  getTagsFromChangedFile,
+  isTagExisting,
+  getChangeFilesReadmeMap,
+} from "../utils";
 
 @suite
 class UtilsTest {
-
-  @test async "TestGetOpenapiTypeDataplane"() {
-    let openapiType = await utils.getOpenapiType("./src/tests/Resource/openapi-type-data-plane-readme.md")
-    assert.equal(openapiType, "data-plane")
+  @test async TestGetOpenapiTypeDataplane() {
+    let openapiType = await utils.getOpenapiType(
+      "./src/tests/Resource/openapi-type-data-plane-readme.md"
+    );
+    assert.equal(openapiType, "data-plane");
   }
 
-  @test async "TestGetOpenapiTypeDataplanArm"() {
-    let openapiType = await utils.getOpenapiType("./src/tests/Resource/openapi-type-arm-readme.md")
-    assert.equal(openapiType, "arm")
+  @test async TestGetOpenapiTypeDataplanArm() {
+    let openapiType = await utils.getOpenapiType(
+      "./src/tests/Resource/openapi-type-arm-readme.md"
+    );
+    assert.equal(openapiType, "arm");
   }
 
-  @test async "TestGetOpenapiTypeNoExistFile"() {
-    let openapiType = await utils.getOpenapiType("C:/code/data-plane/test/readme.md")
-    assert.equal(openapiType, "default")
+  @test async TestGetOpenapiTypeNoExistFile() {
+    let openapiType = await utils.getOpenapiType(
+      "C:/code/data-plane/test/readme.md"
+    );
+    assert.equal(openapiType, "default");
   }
 
-  @test async "TestGetOpenapiTypeFromPathWithArm"() {
-    let openapiType = await utils.getOpenapiType("C:/specification/test/resource-manager/test/readme.md")
-    assert.equal(openapiType, "arm")
+  @test async TestGetOpenapiTypeFromPathWithArm() {
+    let openapiType = await utils.getOpenapiType(
+      "C:/specification/test/resource-manager/test/readme.md"
+    );
+    assert.equal(openapiType, "arm");
   }
 
-  @test async "TestGetOpenapiTypeFromPathWithDataPlane"() {
-    let openapiType = await utils.getOpenapiType("/home/work/1/spec/specification/test/data-plane/test/readme.md")
-    assert.equal(openapiType, "data-plane")
+  @test async TestGetOpenapiTypeFromPathWithDataPlane() {
+    let openapiType = await utils.getOpenapiType(
+      "/home/work/1/spec/specification/test/data-plane/test/readme.md"
+    );
+    assert.equal(openapiType, "data-plane");
   }
 
-  @test async "TestGetOpenapiTypeFromRelativePath"() {
-    let openapiType = await utils.getOpenapiType("specification/test/data-plane/test/readme.md")
-    assert.equal(openapiType, "data-plane")
+  @test async TestGetOpenapiTypeFromRelativePath() {
+    let openapiType = await utils.getOpenapiType(
+      "specification/test/data-plane/test/readme.md"
+    );
+    assert.equal(openapiType, "data-plane");
   }
 
-  @test @timeout(60000) async "TestDoOnTargetBranch"() {
-    const rootName = 'test-root'
-    const repoName = 'mock-repo'
+  @test @timeout(60000) async TestDoOnTargetBranch() {
+    const rootName = "test-root";
+    const repoName = "mock-repo";
     const cfg = await createDevOpsEnv(rootName, repoName);
     /** Create a mock pr.
      * The pr contains two branches.
@@ -66,26 +80,31 @@ class UtilsTest {
      * │   ├── file3.json
      * │   └── file4.json (new file)
      * └── textfile.txt
-     * 
+     *
      * */
-    const pr = await devOps.createPullRequestProperties(cfg)
+    const pr = await devOps.createPullRequestProperties(cfg);
 
-    const files = ['specification/file1.json', 'specification/file2.json', 'specification/file3.json', 'specification/file4.json']
+    const files = [
+      "specification/file1.json",
+      "specification/file2.json",
+      "specification/file3.json",
+      "specification/file4.json",
+    ];
     if (pr !== undefined) {
       const newSwaggers = await utils.doOnTargetBranch(pr, async () => {
-        return files.filter(s => !fs.existsSync(s))
-      })
-      assert.deepEqual(newSwaggers, ['specification/file4.json'])
+        return files.filter((s) => !fs.existsSync(s));
+      });
+      assert.deepEqual(newSwaggers, ["specification/file4.json"]);
     }
 
-    await cleanUp(rootName, repoName)
+    await cleanUp(rootName, repoName);
   }
 
-  @test "TestGetLintVersion" () {
-    process.env.CLASSIC_LINT_VERSION = "1.1.0"
-    process.env.LINT_VERSION = "1.1.0"
-    let version = utils.getLinterVersion()
-    assert.deepEqual(version, { classic: "1.1.0", present: "1.1.0" })
+  @test TestGetLintVersion() {
+    process.env.CLASSIC_LINT_VERSION = "1.1.0";
+    process.env.LINT_VERSION = "1.1.0";
+    let version = utils.getLinterVersion();
+    assert.deepEqual(version, { classic: "1.1.0", present: "1.1.0" });
   }
 
   @test async TestgetTagsFromChangedFile() {
@@ -132,7 +151,7 @@ class UtilsTest {
       "package-2020-06"
     );
     assert.equal(result, false);
-    
+
     // test non-existing readme
     result = isTagExisting(
       "specification/network/resource-manager/readme1.md",
@@ -143,22 +162,59 @@ class UtilsTest {
   }
 
   @test async TestGetChangeFilesReadmeMap() {
-     let cwd = process.cwd();
-     process.chdir("./src/tests/Resource");
+    let cwd = process.cwd();
+    process.chdir("./src/tests/Resource");
 
-     let result = await getChangeFilesReadmeMap([
-       "specification/network/resource-manager/readme.md",
-       "specification/network/resource-manager/Microsoft.Network/stable/2020-03-01/privateEndpoint.json",
-       "profiles/network/resource-manager/Microsoft.Network/a.json"
-     ]);
-     const expect = new Map<string, string[]>([
-       [
-         "specification/network/resource-manager/readme.md",
-         ["specification/network/resource-manager/Microsoft.Network/stable/2020-03-01/privateEndpoint.json"],
-       ],
-     ]);
-     assert.deepEqual(result, expect);
-     process.chdir(cwd);
+    let result = await getChangeFilesReadmeMap([
+      "specification/network/resource-manager/readme.md",
+      "specification/network/resource-manager/Microsoft.Network/stable/2020-03-01/privateEndpoint.json",
+      "profiles/network/resource-manager/Microsoft.Network/a.json",
+    ]);
+    const expect = new Map<string, string[]>([
+      [
+        "specification/network/resource-manager/readme.md",
+        [
+          "specification/network/resource-manager/Microsoft.Network/stable/2020-03-01/privateEndpoint.json",
+        ],
+      ],
+    ]);
+    assert.deepEqual(result, expect);
+    process.chdir(cwd);
   }
 
+  @test TestTrimSwaggerPath() {
+    assert.deepEqual(
+      utils.getRelativeSwaggerPathToRepo(
+        "file:///home/ruowan/work/c93b354fd9c14905bb574a8834c4d69b/specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json:150:5"
+      ),
+      "specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json:150:5"
+    );
+
+    assert.deepEqual(
+      utils.getRelativeSwaggerPathToRepo(
+        "file:///tmp/resolved/specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json:191:5"
+      ),
+      "specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json:191:5"
+    );
+    assert.deepEqual(
+      utils.getRelativeSwaggerPathToRepo("tmp:a/b/readme.md"),
+      "tmp:a/b/readme.md"
+    );
+  }
+
+  @test TestTrimGithubStyleFilePath() {
+    assert.deepEqual(
+      utils.getGithubStyleFilePath(
+        "file:///home/ruowan/work/c93b354fd9c14905bb574a8834c4d69b/specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json:150:5"
+      ),
+      "file:///home/ruowan/work/c93b354fd9c14905bb574a8834c4d69b/specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json#L150:5"
+    );
+
+    assert.deepEqual(
+      utils.getGithubStyleFilePath(
+        "file:///home/ruowan/work/c93b354fd9c14905bb574a8834c4d69b/specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json"
+      ),
+      "file:///home/ruowan/work/c93b354fd9c14905bb574a8834c4d69b/specification/redis/resource-manager/Microsoft.Cache/preview/2019-07-01/redis.json"
+    );
+  }
 }
